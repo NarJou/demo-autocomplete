792373ab23fc56db8adb5830947a04c7709e25f1
NarJou <marinenargisse@gmail.com>
2017-12-21 21:30:07 +0100

    Insert-datas-to-mongodb script: Fixed the None key behavior

--- insert-datas-to-mongodb.py
+++ insert-datas-to-mongodb.py
@@ -9,16 +9,16 @@ products = json.load(f)
 
 for product in products:
   db.insert_one({
-    "sku":product['sku'],
-    "name":product['name'],
-    "type":product['type'],
-    "price":product['price'],
-    "upc":product['upc'],
-    "category":product['category'],
-    "shipping":product['shipping'],
-    "description":product['description'],
-    "manufacturer":product['manufacturer'],
-    "model":product['model'],
-    "url":product['url'],
-    "image":product['image']
+    "sku":product.get("sku", None),
+    "name":product.get("name", None),
+    "type":product.get("type", None),
+    "price":product.get("price", None),
+    "upc":product.get("upc", None),
+    "category":product.get("category", None),
+    "shipping":product.get("shipping", None),
+    "description":product.get("description", None),
+    "manufacturer":product.get("manufacturer", None),
+    "model":product.get("model", None),
+    "url":product.get("url", None),
+    "image":product.get("image", None)
   })
