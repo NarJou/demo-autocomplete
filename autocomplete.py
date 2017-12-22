1e54515f89019596886dda3def3974064b4c6e7e
NarJou <marinenargisse@gmail.com>
2017-12-22 01:12:39 +0100

    Update autocomplete script

---
+++ autocomplete.py
@@ -0,0 +1,39 @@
+import redis
+
+r = redis.StrictRedis(host="35.192.211.192", port=6379, db=0)
+
+print "Loading entries in the Redis DB\n"
+f = open('female-names-2.txt',"r")
+for line in f:
+    n = line.strip()
+    for l in range(1,len(n)):
+        prefix = n[0:l]
+        r.zadd('compl',0,prefix)
+    r.zadd('compl',0,n+"*")
+
+def complete(r,prefix,count):
+    results = []
+    rangelen = 50 # This is not random, try to get replies < MTU size
+    start = r.zrank('compl',prefix)
+    if not start:
+         return []
+    while (len(results) != count):
+         range = r.zrange('compl',start,start+rangelen-1)
+         start += rangelen
+         if not range or len(range) == 0:
+             break
+         for entry in range:
+             minlen = min(len(entry),len(prefix))
+             if entry[0:minlen] != prefix[0:minlen]:
+                count = len(results)
+                break
+             if entry[-1] == "*" and len(results) != count:
+                results.append(entry[0:-1])
+
+    return results
+
+def autoComplete():
+    print complete(r,"abig",50)
+
+if __name__ == "__main__":
+    autoComplete()
